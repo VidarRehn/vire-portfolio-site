68505497a59d20db81db52623a41cd88c03aefc9
Vidar Rehn <vidar.rehn@gmail.com>
2021-12-19 11:23:57 +0100

    included hide-function for footer in active hamburger menu

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 let hamburger = document.querySelector(".hamburger");
 let mainBody = document.querySelector(".main");
 let navBar = document.querySelector(".nav");
+let footer = document.querySelector(".footer");
 
 hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("change");
     mainBody.classList.toggle("hide");
     navBar.classList.toggle("hide");
+    footer.classList.toggle("hide");
 })
